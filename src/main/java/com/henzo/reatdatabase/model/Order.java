3b9718cb22ac3e77c0079d35d24432e117ed6c72
Chibuikem Okonkwo <okonkwo.chibuikem@yahoo.com>
2023-04-24 13:38:16 +0100

    Add Order Model Class

---
+++ src/main/java/com/henzo/reatdatabase/model/Order.java
@@ -0,0 +1,62 @@
+package com.henzo.reatdatabase.model;
+
+public class Order {
+
+	private int orderId;
+	private int foodMenuId;
+	private int customerId;
+	private int reservationId;
+	private int quantity;
+	private double price;
+	private String orderStatus;
+	private String entryDate;
+	
+	public int getOrderId() {
+		return orderId;
+	}
+	public void setOrderId(int orderId) {
+		this.orderId = orderId;
+	}
+	public int getFoodMenuId() {
+		return foodMenuId;
+	}
+	public void setFoodMenuId(int foodMenuId) {
+		this.foodMenuId = foodMenuId;
+	}
+	public int getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+	public int getReservationId() {
+		return reservationId;
+	}
+	public void setReservationId(int reservationId) {
+		this.reservationId = reservationId;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public String getOrderStatus() {
+		return orderStatus;
+	}
+	public void setOrderStatus(String orderStatus) {
+		this.orderStatus = orderStatus;
+	}
+	public String getEntryDate() {
+		return entryDate;
+	}
+	public void setEntryDate(String entryDate) {
+		this.entryDate = entryDate;
+	}
+}
