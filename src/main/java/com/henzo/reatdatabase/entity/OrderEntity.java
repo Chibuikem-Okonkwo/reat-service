580c19d5c398c2101d0d1204e12adf06754e9788
Chibuikem Okonkwo <okonkwo.chibuikem@yahoo.com>
2023-04-24 13:43:21 +0100

    Add Order Entity Class

---
+++ src/main/java/com/henzo/reatdatabase/entity/OrderEntity.java
@@ -0,0 +1,93 @@
+package com.henzo.reatdatabase.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="orders")
+public class OrderEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int orderId;
+	private int foodMenuId;
+	private int customerId;
+	private int reservationId;
+	private int quantity;
+	private double price;
+	private String orderStatus;
+	private String entryDate;
+	
+	public OrderEntity() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getOrderId() {
+		return orderId;
+	}
+
+	public void setOrderId(int orderId) {
+		this.orderId = orderId;
+	}
+
+	public int getFoodMenuId() {
+		return foodMenuId;
+	}
+
+	public void setFoodMenuId(int foodMenuId) {
+		this.foodMenuId = foodMenuId;
+	}
+
+	public int getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+
+	public int getReservationId() {
+		return reservationId;
+	}
+
+	public void setReservationId(int reservationId) {
+		this.reservationId = reservationId;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public String getOrderStatus() {
+		return orderStatus;
+	}
+
+	public void setOrderStatus(String orderStatus) {
+		this.orderStatus = orderStatus;
+	}
+
+	public String getEntryDate() {
+		return entryDate;
+	}
+
+	public void setEntryDate(String entryDate) {
+		this.entryDate = entryDate;
+	}
+	
+}
