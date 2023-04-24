671e3bf99b4137b1588256e6fa6b32697d6e96da
Chibuikem Okonkwo <okonkwo.chibuikem@yahoo.com>
2023-04-24 12:42:18 +0100

    Add ReservationDetails Entity Class

---
+++ src/main/java/com/henzo/reatdatabase/controller/ReservationDetailEntity.java
@@ -0,0 +1,92 @@
+package com.henzo.reatdatabase.controller;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="reservation_details")
+public class ReservationDetailEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int reservationId;
+	private int tableId;
+	private int customerId;
+	private int size;
+	private String reservationDate;
+	private String startTime;
+	private String occasion;
+	private String entryDate;
+	
+	public ReservationDetailEntity() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getReservationId() {
+		return reservationId;
+	}
+
+	public void setReservationId(int reservationId) {
+		this.reservationId = reservationId;
+	}
+
+	public int getTableId() {
+		return tableId;
+	}
+
+	public void setTableId(int tableId) {
+		this.tableId = tableId;
+	}
+
+	public int getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	public void setSize(int size) {
+		this.size = size;
+	}
+
+	public String getReservationDate() {
+		return reservationDate;
+	}
+
+	public void setReservationDate(String reservationDate) {
+		this.reservationDate = reservationDate;
+	}
+
+	public String getStartTime() {
+		return startTime;
+	}
+
+	public void setStartTime(String startTime) {
+		this.startTime = startTime;
+	}
+
+	public String getOccasion() {
+		return occasion;
+	}
+
+	public void setOccasion(String occasion) {
+		this.occasion = occasion;
+	}
+
+	public String getEntryDate() {
+		return entryDate;
+	}
+
+	public void setEntryDate(String entryDate) {
+		this.entryDate = entryDate;
+	}
+}
