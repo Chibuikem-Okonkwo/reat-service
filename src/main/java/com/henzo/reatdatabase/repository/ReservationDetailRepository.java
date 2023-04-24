2e1ce3e241ab156f97d70f5b5a4c0e8efedf0190
Chibuikem Okonkwo <okonkwo.chibuikem@yahoo.com>
2023-04-24 12:44:58 +0100

    Add ReservationDetails Repository Interface

---
+++ src/main/java/com/henzo/reatdatabase/repository/ReservationDetailRepository.java
@@ -0,0 +1,11 @@
+package com.henzo.reatdatabase.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.henzo.reatdatabase.controller.ReservationDetailEntity;
+
+@Repository
+public interface ReservationDetailRepository extends JpaRepository<ReservationDetailEntity, Integer> {
+
+}
